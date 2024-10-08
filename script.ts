635b042aab2e89631c62a62bfd12cb98dde929c9
deepyes02 <deepesh.dhakal@digitalwallet.co.jp>
2024-10-08 17:49:50 +0900

    First commit

--- script.ts
+++ script.ts
@@ -37,32 +37,33 @@ class ThreeJsInit {
 	private restOfTheCodeWeShouldRefactorLater() {
 		// Create a simple cube geometry and material
 		// (() => {
-			const segments = 4;
+			const segments = 12;
 			const geometry = new SphereGeometry(20, segments, segments);
+			geometry.computeVertexNormals();
 			const textureLoader = new TextureLoader();
-			const texture = textureLoader.load('./earth-nasa.jpg');
-
-
-			const material = new MeshStandardMaterial({ map: texture });
-			const sphere = new Mesh(geometry, material);
+			let sphere : any;
+			const texture = textureLoader.load('./earth-nasa.jpg', (texture) => {
+			console.log(texture)
+			});
+			const material = new MeshStandardMaterial({ map: texture, roughness: 0.5, metalness: 0.2 });
+			sphere = new Mesh(geometry, material);
 			this.scene.add(sphere);
-		// })();
 
-		// Let's add a circle
-		// (() => {
-		// 	const circleGeometry = new CircleGeometry(20, 64);
-		// 	const circleMaterial = new MeshBasicMaterial({ color: 'green' });
-		// 	const circleMesh = new Mesh(circleGeometry, circleMaterial);
-		// 	this.scene.add(circleMesh)
-		// })();
+
+		(() => {
+			const circleGeometry = new CircleGeometry(20, 32);
+			const circleMaterial = new MeshBasicMaterial({ color: 'green' });
+			const circleMesh = new Mesh(circleGeometry, circleMaterial);
+			this.scene.add(circleMesh)
+		})();
 
 		// Add Point Light
 		(() => {
 			// Create a PointLight
-			const light = new PointLight(0xff00ff, 1, 200, 2);
+			const light = new PointLight(0xff00ff, 100, 2, 2);
 
 			// Set the position of the light
-			light.position.set(0, 0, 0); // Moved away from the origin
+			light.position.set(10, 30, 1000); // Moved away from the origin
 
 			// Optionally, add a light helper to visualize the light position
 			const lightHelper = new PointLightHelper(light, 1);
@@ -79,11 +80,11 @@ class ThreeJsInit {
 		})()
 
 		// Set the camera position
-		this.camera.position.z = 150;
+		this.camera.position.z = 250;
 
 		// Animation parameters
 		let scrollY = 0;
-		let maxScale = 200;
+		let maxScale = 100;
 		let scrollMax = window.innerHeight * 1 // Adjust as needed for your scroll height
 
 		const animate = () => {
@@ -92,9 +93,10 @@ class ThreeJsInit {
 
 			scrollY = window.scrollY;
 			const progress = Math.min(scrollY / scrollMax, 1);
-			const scale = progress * (maxScale + 2)
-			sphere.scale.set(scale, scale, scale)
-			sphere.rotation.set(0, scale / 10, 0);
+			const scale = progress * (maxScale + 2) / 10
+			// sphere.scale.set(scale, scale, scale)
+			sphere.rotation.set(0.2, scale, 0.2);
+			// this.camera.position.z+= progress;
 			if (ThreeJsInit.debug) {
 				ThreeJsInit.debug.innerHTML = `scrollY => ${scrollY}, progress => ${(progress * 100)}, scale => ${scale}`
 			};
